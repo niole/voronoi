d8790dba4ba05e1a17f6c4fc53c3505fcbb53f70
Niole Nelson <niolenelson@gmail.com>
2015-10-09 12:21:38 -0400

    adds a little vornoi that redraws on mousemove

--- js/app.js
+++ js/app.js
@@ -1,44 +1,24 @@
-console.log('entry point');
 var d3 = require('d3');
 var $ = require('jquery');
+var _ = require('lodash');
+var Voronoi = require('./voronoi');
 
 $(document).ready(function() {
 
+draw();
+
+
+function draw() {
   var padding = 10,
-      width = 960,
-      height = 500;
+      width = $(window).width(),
+      height = $(window).height();
 
-  var points = [
-    [20, 20],
-    [701, 30],
-    [400, 200],
-    [760, 300],
-    [200, 200]
-  ];
+  var points = [];
+  for (var i=0; i<100; i++) {
+    points.push([Math.random()*width, Math.random()*height]);
+  }
 
-  var voronoi = d3.geom.voronoi()
-      .clipExtent([[padding, padding], [width - padding, height - padding]]);
-
-  var color = d3.scale.category20c();
-
-  var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height);
-
-  svg.selectAll("path")
-      .data(voronoi(points))
-    .enter().append("path")
-      .style("fill", function(d, i) { return color(i); })
-      .style("opacity", .5)
-      .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-      .on('click', function() {
-        console.log('stuff');
-      });
-
-  svg.selectAll("circle")
-      .data(points)
-    .enter().append("circle")
-      .style("fill", function(d, i) { return color(i); })
-      .attr("transform", function(d) { return "translate(" + d + ")"; })
-      .attr("r", 4.5);
+  var V = new Voronoi(points, padding, width, height);
+  V.draw();
+}
 });
---
+++ js/voronoi.js
@@ -0,0 +1,71 @@
+var d3 = require('d3');
+var $ = require('jquery');
+
+module.exports = (function() {
+  function Voronoi(points, padding, width, height) {
+    var self = this;
+    this.data = points;
+    this.padding = padding;
+
+    this.width = width;
+    this.height = height;
+    this.active = false;
+
+
+    this.svg = d3.select("body").append("svg")
+        .attr("width", this.width)
+        .attr("height", this.height)
+        .on('mouseleave', function() {
+          this.active = false;
+          this.data.pop();
+        }.bind(this))
+        .on('mousemove', function() {
+          var newPoint = d3.mouse(this);
+          self.update(newPoint[0], newPoint[1]);
+        });
+}
+
+Voronoi.prototype.update = function(x,y) {
+  if (this.active) {
+    this.data[this.data.length-1][1] = y;
+    this.data[this.data.length-1][0] = x;
+  } else {
+    this.active = true;
+    this.data.push([x,y]);
+  }
+  this.draw();
+};
+
+
+Voronoi.prototype.draw = function() {
+
+    this.voronoi = d3.geom.voronoi()
+        .clipExtent([[this.padding, this.padding], [this.width - this.padding, this.height - this.padding]]);
+
+    var color = d3.scale.category20c();
+
+
+
+    this.V = this.svg.selectAll("path")
+              .data(this.voronoi(this.data));
+
+    this.V
+      .enter()
+      .append("path");
+
+      this.V
+          .select("path");
+
+      this.V
+        .style("fill", function(d, i) { return color(i); })
+        .style("opacity", .5)
+        .attr("d", function(d) { return "M" + d.join("L") + "Z"; });
+
+      this.V
+          .exit()
+          .remove();
+};
+
+return Voronoi;
+}())
+
