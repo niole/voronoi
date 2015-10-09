e52e987ac69e8659eaa79e9b80e3981bbc72b0a4
Niole Nelson <niolenelson@gmail.com>
2015-10-08 23:25:53 -0400

    creates basic voronoi

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 console.log('entry point');
+var d3 = require('d3');
+var $ = require('jquery');
+
+$(document).ready(function() {
+
+  var padding = 10,
+      width = 960,
+      height = 500;
+
+  var points = [
+    [200, 200],
+    [760, 300]
+  ];
+
+  var voronoi = d3.geom.voronoi()
+      .clipExtent([[padding, padding], [width - padding, height - padding]]);
+
+  var color = d3.scale.category10();
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  svg.selectAll("path")
+      .data(voronoi(points))
+    .enter().append("path")
+      .style("fill", function(d, i) { return color(i); })
+      .attr("d", function(d) { return "M" + d.join("L") + "Z"; });
+
+  svg.selectAll("circle")
+      .data(points)
+    .enter().append("circle")
+      .style("fill", function(d, i) { return color(i); })
+      .attr("transform", function(d) { return "translate(" + d + ")"; })
+      .attr("r", 4.5);
+});
