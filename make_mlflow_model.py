a6d402907b6036a3e83fa60fee2616f3dcf025e2
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2026-09-09 12:52:09 -0700

    Pushed from Domino: https://nio64t138448.engineering-dev.domino.tech/u/integration-test/sshtest/workspaces?executionId=6a837403c79aa065908a1d96

---
+++ make_mlflow_model.py
@@ -0,0 +1,46 @@
+## This script uses MLflow version 2 SDK. I think this should be compatible with version 3 as well.
+## DSE should have all requirements for this already installed.
+## This script creates and registers a simple sklearn model in MLflow Model Registry.
+##
+## Usage: python register_mlflow_model.py <registered_model_name>
+##
+from sklearn.datasets import make_regression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+import mlflow
+import mlflow.sklearn
+from mlflow.models import infer_signature
+import sys
+
+with mlflow.start_run() as run:
+    X, y = make_regression(n_features=4, n_informative=2, random_state=0, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    params = {"max_depth": 2, "random_state": 42}
+    model = RandomForestRegressor(**params)
+    model.fit(X_train, y_train)
+
+    # Infer the model signature
+    y_pred = model.predict(X_test)
+    signature = infer_signature(X_test, y_pred)
+
+    # Log parameters and metrics using the MLflow APIs
+    mlflow.log_params(params)
+    mlflow.log_metrics({"mse": mean_squared_error(y_test, y_pred)})
+
+    registered_model_name = sys.argv[1] if len(sys.argv) > 1 else None
+    if not registered_model_name:
+        raise ValueError("registered_model_name argument is required")
+
+    # Log the sklearn model and register as version 1
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="sklearn-model",
+        signature=signature,
+        registered_model_name=registered_model_name,
+    )
+
+
